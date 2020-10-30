9698f1d80566058b22b00294c94ab83f25f1e7af
Nicolaibouquin <56248805+Nicolaibouquin@users.noreply.github.com>
2020-10-30 15:22:03 +0100

    funfact

--- Server/src/User.java
+++ Server/src/User.java
@@ -1,39 +1,55 @@
 import java.util.Random;
 
 public class User {
-	
-	
-	public static String username;
-	public static String password;
-	public static int id;
-	
-	
-	User(String username, String password){
-		this.username = username;
-		this.password = password;
-		
-		Random cd = new Random();
-		id = cd.nextInt();
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public int getId() {
-		return id;
-	}
+
+  // An attribute that stores the username of the user
+  private String username;
+
+  // An attribute that stores the password of the user
+  private String password;
+
+  // An attribute that stores the id of the user
+  private String id;
+
+    // A constructor the creates a User object with a specified username and password
+    public User(String username, String password){
+
+        this.username = username;
+        this.password = password;
+
+        // A random integer is assigned as the id
+        Random cd = new Random();
+        id = Integer.toString(cd.nextInt());
+    }
+
+    // a method that return the username of the user
+    public String getUsername() {
+        return username;
+    }
+
+    // a method that changes the username of the user
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // a method that returns the password
+    public String getPassword() {
+        return password;
+    }
+
+    // a method that changes the password of the user
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // a method that returns that id of the user
+    public String getId() {
+        return id;
+    }
+
+    // A method that changes the id of the user
+    public void setId(String id) {
+        this.id = id;
+    }
 
 }
